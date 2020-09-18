aea566f512b0cdbc6cbbca300ac50347d9c49e23
binbin <chenbin@qq.com>
2020-09-18 19:53:29 +0800

    9/18

--- yzf.sql
+++ yzf.sql
@@ -1,34 +1,17 @@
---
--- Database: `yzf`
---
-CREATE DATABASE IF NOT EXISTS `yzf` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `yzf`;
-
--- --------------------------------------------------------
---
--- 表的结构 `yzf_user`
---
-
-DROP TABLE IF EXISTS `yzf_user`;
-CREATE TABLE `yzf_user` (
-  `uid` int(11) NOT NULL,
-  `uname` varchar(32) DEFAULT NULL,
-  `upwd` varchar(32) DEFAULT NULL,
-  `email` varchar(64) DEFAULT NULL,
-  `phone` varchar(16) DEFAULT NULL,
-  `avatar` varchar(128) DEFAULT NULL,
-  `user_name` varchar(32) DEFAULT NULL,
-  `gender` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- 插入之前先把表清空（truncate） `yzf_user`
---
-
-TRUNCATE TABLE `yzf_user`;
---
--- 转存表中的数据 `yzf_user`
---
-
-INSERT INTO `yzf_user` (`uid`, `uname`, `upwd`, `email`, `phone`, `avatar`, `user_name`, `gender`) VALUES
-(1, 'dingding', '123456', 'ding@qq.com', '13501234567', 'img/avatar/default.png', '丁伟', 1);
+#设置客户端连接服务器的编码
+SET NAMES UTF8;
+#丢弃数据库如果存在
+DROP DATABASE IF EXISTS yzf;
+#创建数据库，设置储存的编码
+CREATE DATABASE yzf CHARSET=UTF8;
+#进入创建的数据库
+USE yzf;
+#创建用户表user
+CREATE TABLE `user` (
+  `uid` mediumint(8) unsigned NOT NULL AUTO_INCRMENT COMMENT '用户ID,主键且自增 不为空',
+  `uname` VARCHAR(16),
+  `email` VARCHAR(16),
+  `phone` TINYINT(11),
+  `avatar` VARCHAR(50) NOT NULL DEFAULT `unamed.jpg` COMMENT `用头像`，
+  `yzm`
+);
